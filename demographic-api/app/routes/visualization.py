9b1aebac1b67499aca569b9a8688eaea6ec44bd3
Nodir Kosimkhujaev <128922164+zSayid@users.noreply.github.com>
2025-03-27 04:26:33 -0500

    Update visualization.py
    updated

--- demographic-api/app/routes/visualization.py
+++ demographic-api/app/routes/visualization.py
@@ -1,5 +1,5 @@
-import numpy as np # malumotga ishlov berish uchun
-import matplotlib.pyplot as plt # malumotni vizualizatsiya qilish uchun
+import numpy as np 
+import matplotlib.pyplot as plt 
 from fastapi import APIRouter, HTTPException
 from io import BytesIO
 import base64
@@ -15,13 +15,8 @@ def get_population_graph(country: str):
     Grafik PNG formatida qaytariladi.
     """
     
-    # tasodifiy aholining ma'lumotlarini yaratamiz (real malumotlarni keyin bazadan olamiz)
-    
     years = np.arange(1970, 2021, 1) # 1970-yildan 2020-yilgacha
-    population = np.cumsum(np.random.randint(50000, 200000, size=len(years))) + random.randint(5_000_000, 10_000_000)
-    
-    
-    # Grafikni yaratamiz va uni PNG formatida saqlaymiz
+    population = np.cumsum(np.random.randint(50000, 200000, size=len(years))) + random.randint(5_000_000, 
     
     buf = BytesIO()
     plt.savefig(buf, format="png")
